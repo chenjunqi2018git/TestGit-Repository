0a187f9400e4d30ff1e2d43a528ab65ed8b0e75a
chenjunqi <chenjunqi2016.ok@gmail.com>
2018-05-09 03:02:36 +0900

    change 444444
    Signed-off-by: chenjunqi <chenjunqi2016.ok@gmail.com>

--- TestGitProject/src/test/show/tools/MainClass.java
+++ TestGitProject/src/test/show/tools/MainClass.java
@@ -4,10 +4,12 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test git From MainClass!!");
+		System.out.println("test1111 git From MainClass!!");
 
 		TestClass testClass = new TestClass();
 		testClass.show();
+		System.out.println("test2222 git From MainClass!!");
+		
 	}
 
 }
