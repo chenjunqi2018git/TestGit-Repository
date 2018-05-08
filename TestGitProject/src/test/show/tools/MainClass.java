a28f7eecd3884a63683620fe79b8aa21b54aa024
chenjunqi <chenjunqi2016.ok@gmail.com>
2018-05-09 02:41:42 +0900

    first commit

---
+++ TestGitProject/src/test/show/tools/MainClass.java
@@ -0,0 +1,17 @@
+package test.show.tools;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test git From MainClass!!");
+
+		
+		
+		
+		
+		TestClass testClass = new TestClass();
+		testClass.show();
+	}
+
+}
---
+++ TestGitProject/src/test/show/tools/TestClass.java
@@ -0,0 +1,7 @@
+package test.show.tools;
+
+public class TestClass {
+	public void show() {
+		System.out.println("show message from TestClass");
+	}
+}
