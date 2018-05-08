41f5f5a394c8cf0e02763bff3e19436a3aa68869
chenjunqi <chenjunqi2016.ok@gmail.com>
2018-05-09 03:02:36 +0900

    change 3

--- TestGitProject/src/test/show/tools/MainClass.java
+++ TestGitProject/src/test/show/tools/MainClass.java
@@ -4,10 +4,11 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test git From MainClass!!");
+		System.out.println("test11 git From MainClass!!");
 
 		TestClass testClass = new TestClass();
 		testClass.show();
+		System.out.println("test22 git From MainClass!!");
 	}
 
 }
