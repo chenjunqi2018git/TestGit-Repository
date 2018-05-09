c5b7ba7ce06c92c9ac39fc8c610872de6974e4bc
chenjunqi <chins@ns.aucnet.co.jp>
2018-05-09 14:34:50 +0900

    ０５０９修正

--- TestGitProject/src/test/show/tools/MainClass.java
+++ TestGitProject/src/test/show/tools/MainClass.java
@@ -6,16 +6,16 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("test1111 git From MainClass!!");
 
-
-
 		TestClass testClass = new TestClass();
 		testClass.show();
 
 		System.out.println("test2222 git From MainClass!!");
 
-		//web edit
-		//local merged
-//web edit222
+		// web edit
+		// local merged
+		// web edit222
+
+		System.out.println("test3333 git From MainClass!!");// develop_chenjunqiで修正しました。
 	}
 
 }
