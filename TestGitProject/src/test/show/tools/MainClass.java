35ce7cc62c7bacba78aa6ca5ace4b95e3f310cb4
chenjunqi2018git <39099199+chenjunqi2018git@users.noreply.github.com>
2018-05-10 23:26:20 +0900

    ssss

--- TestGitProject/src/test/show/tools/MainClass.java
+++ TestGitProject/src/test/show/tools/MainClass.java
@@ -3,14 +3,6 @@ package test.show.tools;
 public class MainClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stubwwww
-=======dd
-		// TODO Auto-generated method stubssss
->>>>>>> branch 'master' of https://github.com/chenjunqi2018git/TestGit-Repository.git
-
-		System.out.println("test1111 git From MainClass!!");
-//oksadsadqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqssss
 
 		TestClass testClass = new TestClass();
 		testClass.show();
