67b6854db11e538290f1cf3b5b94a00054b1cb75
chenjunqi <chenjunqi2016.ok@gmail.com>
2018-05-10 22:43:37 +0900

    ssssss

--- TestGitProject/src/test/show/tools/MainClass.java
+++ TestGitProject/src/test/show/tools/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test1111 git From MainClass!!");
-
+//ok
 		TestClass testClass = new TestClass();
 		testClass.show();
 		
