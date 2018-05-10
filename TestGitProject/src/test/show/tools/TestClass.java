afdc19ff2029a8d5113a2ebaeb545481a6241d0c
chenjunqi <chenjunqi2016.ok@gmail.com>
2018-05-10 23:23:16 +0900

    asdsadasd

--- TestGitProject/src/test/show/tools/TestClass.java
+++ TestGitProject/src/test/show/tools/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 
 //<<<<<<< HEAD
 		System.out.println("show message from TestClass");
-//oksfasffasd
+//oksfasffasdasdsadsa
 //=======
 		System.out.println("show message from TestClass");//asdfasdfa
 //okasfsafasfsdf
